e6bef6d6d32799a894fb32c0f95b01df2715779f
Robert Samigullin <robert.samigullin.99@gmail.com>
2022-12-21 12:28:18 +0300

    ADD: fixed code, write docker-compose

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "http://localhost/backend";
+
 
 class EmployeeService {
 
